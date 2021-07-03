3c643a0ee13c656b050f8a3ab729c2ca3e96c089
DrFacepalm <lucaslin111@gmail.com>
2021-07-03 13:39:52 +1000

    Initial DaisukiSutoka Commit

---
+++ DaisukiSutoka/README.md
@@ -0,0 +1,11 @@
+# DaisukuSutoka
+
+This is a discord bot that will stalk the Daisuki bot in a server, and record stats about players.
+
+
+## Development
+
+**Starting the venv**
+```bash
+source env/bin/activate # linux
+```
---
+++ DaisukiSutoka/bot.py
@@ -0,0 +1,16 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')   
+
+client.run(TOKEN)
