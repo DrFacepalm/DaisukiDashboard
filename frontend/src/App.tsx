f3e1f0f2ddfe3ffb3968c64260883733fe61d258
xpirep <justin.or.mail@gmail.com>
2021-07-18 23:31:14 +1000

    feat(theme): theme works flawlessly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { default as myData } from "./data/tp_min.json";
-import { Typography, Container, Card } from "@material-ui/core";
+import {
+  Typography,
+  Container,
+  Card,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import useLocalStorage from "./utils/LocalStorageHook";
 import ThemeProvider from "./themes/ThemeProvider";
 // import { createGlobalStyle } from "styled-components";
 // import "./App.css";
+import themesList from "./themes/_list.json";
+type themesType = {
+  name: string;
+  bgColor: string;
+  textColor: string;
+};
 
 const Header = styled.div`
   padding: 40px 0px;
@@ -17,8 +30,13 @@ const StyledBackground = styled.div`
 `;
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme", "8008");
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setTheme(event.target.value as string);
+  };
   return (
-    <ThemeProvider themeString="8008">
+    <ThemeProvider themeString={theme}>
       <StyledBackground>
         <Container maxWidth="xl">
           <Card elevation={0}>
@@ -30,6 +48,16 @@ function App() {
               </Typography>
             </Header>
           </Card>
+          <Select value={theme} onChange={handleChange}>
+            {themesList.map((theme: themesType) => (
+              <MenuItem
+                style={{ color: theme.textColor, background: theme.bgColor }}
+                value={theme.name}
+              >
+                {theme.name}
+              </MenuItem>
+            ))}
+          </Select>
         </Container>
       </StyledBackground>
     </ThemeProvider>
--- frontend/src/themes/ThemeProvider.tsx
+++ frontend/src/themes/ThemeProvider.tsx
@@ -2,127 +2,11 @@ import React, { useLayoutEffect } from "react";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import { createGlobalStyle } from "styled-components";
 import useLocalStorage from "../utils/LocalStorageHook";
-// import { default as rawThemes } from "./themes.json";
-import * as themes from "./themes.json";
-import { themeKeys } from "./themeKeys";
+import rawThemes from "./themes.json";
 
 type MyThemeProviderProps = {
   children?: React.ReactNode;
-  themeString: //themeKeys;
-  | "8008"
-    | "cyberspace"
-    | "hammerhead"
-    | "miami"
-    | "olive"
-    | "serika_dark"
-    | "80s_after_dark"
-    | "dark"
-    | "honey"
-    | "miami_nights"
-    | "olivia"
-    | "shadow"
-    | "9009"
-    | "dark_magic_girl"
-    | "horizon"
-    | "midnight"
-    | "onedark"
-    | "shoko"
-    | "aether"
-    | "darling"
-    | "iceberg_dark"
-    | "milkshake"
-    | "our_theme"
-    | "solarized_dark"
-    | "alduin"
-    | "deku"
-    | "iceberg_light"
-    | "mint"
-    | "paper"
-    | "solarized_light"
-    | "alpine"
-    | "dollar"
-    | "ishtar"
-    | "mizu"
-    | "pastel"
-    | "sonokai"
-    | "arch"
-    | "dots"
-    | "joker"
-    | "modern_dolch"
-    | "peaches"
-    | "stealth"
-    | "bento"
-    | "dracula"
-    | "laser"
-    | "monokai"
-    | "pulse"
-    | "strawberry"
-    | "bingsu"
-    | "drowning"
-    | "lavender"
-    | "mountain"
-    | "red_dragon"
-    | "striker"
-    | "bliss"
-    | "dualshot"
-    | "lil_dragon"
-    | "mr_sleeves"
-    | "red_samurai"
-    | "superuser"
-    | "blueberry_dark"
-    | "ez_mode"
-    | "lime"
-    | "ms_cupcakes"
-    | "repose_dark"
-    | "sweden"
-    | "blueberry_light"
-    | "fledgling"
-    | "luna"
-    | "muted"
-    | "repose_light"
-    | "taro"
-    | "botanical"
-    | "froyo"
-    | "magic_girl"
-    | "nausea"
-    | "retro"
-    | "terminal"
-    | "bouquet"
-    | "fundamentals"
-    | "mashu"
-    | "nautilus"
-    | "retrocast"
-    | "terra"
-    | "bushido"
-    | "future_funk"
-    | "matcha_moccha"
-    | "nebula"
-    | "rose_pine"
-    | "vaporwave"
-    | "cafe"
-    | "graen"
-    | "matrix"
-    | "night_runner"
-    | "rose_pine_dawn"
-    | "voc"
-    | "camping"
-    | "grand_prix"
-    | "menthol"
-    | "nord"
-    | "rose_pine_moon"
-    | "vscode"
-    | "carbon"
-    | "gruvbox_dark"
-    | "metaverse"
-    | "norse"
-    | "rudy"
-    | "watermelon"
-    | "copper"
-    | "gruvbox_light"
-    | "metropolis"
-    | "oblivion"
-    | "serika"
-    | "wavez";
+  themeString: string;
 };
 
 type themeType = {
@@ -137,17 +21,15 @@ type themeType = {
   "--colorful-error-extra-color"?: string;
 };
 
-// const themes: {
-//   [key: string]: themeType;
-// } = rawThemes;
+const themes: {
+  [key: string]: themeType;
+} = rawThemes;
 
 const get = (key: string) =>
   getComputedStyle(document.documentElement).getPropertyValue(key).trim();
 
-const set = (key: string, value: string) => {
+const set = (key: string, value: string) =>
   document.documentElement.style.setProperty(key, value);
-  console.log(key, value);
-};
 
 const MyThemeProvider = ({ children, themeString }: MyThemeProviderProps) => {
   const GlobalStyle = createGlobalStyle`
@@ -168,12 +50,9 @@ const MyThemeProvider = ({ children, themeString }: MyThemeProviderProps) => {
 
   useLayoutEffect(() => {
     // set the theme based on themeString
-    // const themes = rawThemes as { [key: string]: themeType };
-    console.log(themeString);
-    console.log(themes["8008"]);
-    console.log(themes);
-    themes["8008"] &&
-      Object.entries(themes["8008"]).map((value: [string, string]) =>
+    console.log("setting theme to", themeString);
+    themes[themeString] &&
+      Object.entries(themes[themeString]).map((value: [string, string]) =>
         set(value[0], value[1])
       );
 
